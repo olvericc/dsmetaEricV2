eced07624799de07c30b7eca7528c041a09e8404
Eric Lopes <olv.ericlopes@gmail.com>
2022-09-05 20:54:27 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Hello World!</h1>
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
