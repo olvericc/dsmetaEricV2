409c43a9704a76a1dbac32690c614592ac578e79
Eric Lopes <olv.ericlopes@gmail.com>
2022-09-05 20:05:17 -0300

    Project created

---
+++ backend/src/test/java/com/ericsuperior/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsuperior.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
