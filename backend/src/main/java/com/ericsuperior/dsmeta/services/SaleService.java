50319cd0ccd2ceb8b4e01f9df933e937e131464c
Eric Lopes <olv.ericlopes@gmail.com>
2022-09-07 13:57:13 -0300

    Date select

--- backend/src/main/java/com/ericsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/ericsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.ericsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ericsuperior.dsmeta.entities.Sale;
@@ -15,8 +19,18 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	// Read
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+	
+		LocalDate today = LocalDate.ofInstant(
+				Instant.now(),
+				ZoneId.systemDefault()
+		);
+		
+		// Exceptions
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 	
 }
