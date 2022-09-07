866d6f5406931d4e4c30c2d8bb594cde815c51d5
Eric Lopes <olv.ericlopes@gmail.com>
2022-09-07 14:39:26 -0300

    Twilio SMS

---
+++ backend/src/main/java/com/ericsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,52 @@
+package com.ericsuperior.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.ericsuperior.dsmeta.entities.Sale;
+import com.ericsuperior.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getDayOfMonth() + "/" +
+					  sale.getDate().getMonthValue() + "/" +
+					  sale.getDate().getYear();
+		
+		String msg = "The seller " + sale.getSellerName() + " made "
+					 + sale.getDeals() + " deals on the date " + date + " with a total of US$ "
+					 + String.format("2%.2f", sale.getAmount());
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
